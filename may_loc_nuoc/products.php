1fcef2e26c51460c2dc127df485bcb5937ab4423
dangduong0127 <110780497+dangduong0127@users.noreply.github.com>
2023-03-07 20:51:07 +0700

    index, cart, product follow style of Karofi HP

---
+++ may_loc_nuoc/products.php
@@ -0,0 +1,339 @@
+<?php
+	require_once('layouts/header.php');
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+
+<div class="cover">
+	<div class="shop-page-title">
+		<div class="shop-page-inner">
+			<a href="index.php">TRANG CHỦ</a>
+			<span>/</span>
+			<p>Máy lọc nước karofi</p>
+		</div>
+
+		<div class="orderby">
+			<p>Hiển thị 1 - 12 của 51 kết quả</p>
+			<div class="select">
+				<select class="form-select" aria-label="Default select example">
+				  <option selected>Sắp xếp</option>
+				  <option value="1">Mới nhất</option>
+				  <option value="2">Thứ tự theo mức độ phổ biến</option>
+				  <option value="3">Thứ tự theo điểm đánh giá</option>
+				  <option value="4">Thứ tự theo giá: từ thấp đến cao</option>
+				  <option value="5">Thứ tự theo giá: từ cao đến thấp</option>
+				</select>
+			</div>
+		</div>
+	</div>
+
+
+<div class="main">
+	<div class="box-category-product">
+			<span>DANH MỤC</span>
+			<div class="diviser-small"></div>
+			<a href="">Máy lọc nước karofi</a>
+			<button onclick="myFunction()" style="border: none;background: none;margin-left: 70px;"><i class="fa-solid fa-angle-down"></i></button>
+			<ul id="children" style="display: none;">
+				<a href="">Máy lọc nước bán công nghiệp</a>
+				<a href="">Máy lọc nước để gầm</a>
+				<a href="">Máy lọc nước tủ đứng</a>
+			</ul>
+			<div class="underline"></div>
+
+			<a href="">Máy lọc nước Karofi Livotec</a>
+			<div class="underline"></div>
+
+			<a href="">Máy lọc nước Korihome</a>
+			<div class="underline"></div>
+
+			<a href="">Máy lọc nước Kangaroo</a>
+			<div class="underline"></div>
+
+			<a href="">Máy lọc nước nóng lạnh</a>
+			<button onclick="myFunction1()" style="border: none;background: none;margin-left: 42px;"><i class="fa-solid fa-angle-down"></i></button>
+			<ul id="children-coldwarm" style="display: none;">
+				<a href="">Cây nước nóng lạnh hút bình</a>
+				<a href="">Cây nước nóng lạnh úp bình</a>
+				<a href="">Máy lọc nước nóng lạnh</a>
+			</ul>
+			<div class="underline"></div>
+
+			<a href="">Hệ thống lọc đầu nguồn</a>
+			<div class="underline"></div>
+
+			<a href="">Máy lọc không khí</a>
+			<div class="underline"></div>
+
+			<a href="">Sản phẩm khác</a>
+			<button onclick="myFunction2()" style="border: none;background: none;margin-left: 100px;"><i class="fa-solid fa-angle-down"></i></button>
+			<ul id="children-other" style="display: none;">
+				<a href="">Linh - Phụ kiện</a>
+				<a href="">Quạt và Quạt điều hoà</a>
+			</ul>
+			<div class="underline"></div>
+
+			<a href="">Flash sale</a>
+			<div class="underline"></div>
+			<span>LỌC THEO GIÁ</span>
+			<div class="diviser-small"></div>
+		</div>
+
+		<div class="product-karofi-frame">
+			<div class="product-karofi">
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="single-product">
+					<div class="thumbnail">
+						<div class="discount">-19%</div>
+						<div class="img-preview">
+							<img src="img/kaq-u96-3-large.png">
+						</div>
+						<img src="img/U96-Karofi-Hai-Phong-thang-2-2023-copy-280x280.jpeg">
+						<div class="quick-view">
+							<a href="">Xem nhanh</a>
+						</div>
+					</div>
+					<div class="box-title">
+						<a href="">Máy lọc nước Karofi KAQ-U96</a>
+						<div class="box-title-inner">
+							<del>7.970.00 đ</del> <span>6.350.000 đ</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+</div>
+
+<nav aria-label="..." style="height: 10px;z-index: 99; margin-left: 960px;margin-bottom: 60px;">
+  <ul class="pagination pagination-lg">
+    <li class="page-item disabled" style="margin-left: -70px;">
+      <a class="page-link" href="#" tabindex="-1">1</a>
+    </li>
+    <li class="page-item" style="margin-left: -11px"><a class="page-link" href="#">2</a></li>
+    <li class="page-item" style="margin-left: 50px"><a class="page-link" href="#">3</a></li>
+  </ul>
+</nav>
+
+<script>
+	function myFunction() {
+	  var x = document.getElementById("children");
+	  if (x.style.display === "none") {
+	    x.style.display = "block";
+	  } else {
+	    x.style.display = "none";
+	  }
+	}
+	function myFunction1() {
+	  var x = document.getElementById("children-coldwarm");
+	  if (x.style.display === "none") {
+	    x.style.display = "block";
+	  } else {
+	    x.style.display = "none";
+	  }
+	}
+	function myFunction2() {
+	  var x = document.getElementById("children-other");
+	  if (x.style.display === "none") {
+	    x.style.display = "block";
+	  } else {
+	    x.style.display = "none";
+	  }
+	}
+</script>
+
+<?php
+	require_once('layouts/footer.php');
+?>
